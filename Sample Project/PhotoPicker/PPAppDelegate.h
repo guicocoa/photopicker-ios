8ac23886c4461cfe46f9bc47cb28f3e631e4dd94
Caleb Davenport <calebmdavenport@gmail.com>
2012-07-13 00:14:24 -0400

    add initial sample project

---
+++ Sample Project/PhotoPicker/PPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PPAppDelegate.h
+//  PhotoPicker
+//
+//  Created by Caleb Davenport on 7/13/12.
+//  Copyright (c) 2012 Caleb Davenport. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample Project/PhotoPicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoPicker
+//
+//  Created by Caleb Davenport on 7/13/12.
+//  Copyright (c) 2012 Caleb Davenport. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PPAppDelegate class]));
+    }
+}
